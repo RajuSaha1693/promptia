ee94f86d777f55106ab86d2ce3f3a18e984ec4fb
Raju Saha <rajusaha010693@gmail.com>
2023-08-23 08:25:37 +0530

    updated files and folder structure

---
+++ app/layout.jsx
@@ -0,0 +1,20 @@
+import "@styles/global.css";
+
+export const metadata = {
+  title: "Promptia",
+  description: "Learn Next with help of AI",
+};
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
